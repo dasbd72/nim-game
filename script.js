a21a612828f7966e02fcab435b3d678fa2537cc1
dasbd72 <twbd723@gmail.com>
2024-06-12 22:29:32 +0800

    feat: skeleton of nim game

---
+++ script.js
@@ -0,0 +1,93 @@
+// script.js contains the logic for the Nim game
+
+// onClickStone is called when a stone is clicked
+onClickStone = (pile_id, stone_id) => {
+  console.log("Clicked stone", stone_id, "in pile", pile_id);
+
+  stone = document.getElementById(stone_id);
+  if (!stone) {
+    console.error("Stone not found");
+    return;
+  }
+  stone.classList.toggle("stone-enabled");
+  stone.classList.toggle("stone-disabled");
+};
+
+// addStone adds a stone to the pile
+addStone = (pile, stone_id) => {
+  console.log("Adding stone");
+  stone = document.createElement("div");
+  stone.classList.add("stone");
+  stone.classList.add("stone-enabled");
+  stone.id = stone_id;
+  stone.addEventListener("click", () => onClickStone(pile.id, stone_id));
+  pile.appendChild(stone);
+};
+
+// addPile adds a pile with n_stones to the game container
+addPile = (game_container, pile_id, n_stones) => {
+  pile = document.createElement("div");
+  pile.classList.add("pile");
+  pile.id = pile_id;
+
+  for (let i = 0; i < n_stones; i++) {
+    addStone(pile, `${pile_id}-stone-${i}`);
+  }
+  game_container.appendChild(pile);
+};
+
+// initialize initializes the game with the number of piles and stones specified
+initialize = (n_piles, n_stones) => {
+  console.log(
+    "Initializing game with",
+    n_piles,
+    "piles and",
+    n_stones,
+    "stones"
+  );
+  game_container = document.getElementById("game-container");
+  if (!game_container) {
+    console.error("game-container not found");
+    return;
+  }
+  game_container.innerHTML = "";
+  for (let i = 0; i < n_piles; i++) {
+    addPile(game_container, `pile-${i}`, n_stones);
+  }
+};
+
+// onClickStartGame is called when the start game button is clicked
+// It initializes the game with the number of piles and stones specified by the user
+onClickStartGame = () => {
+  console.log("Starting game");
+  n_piles = document.getElementById("input-num-piles").value;
+  if (!n_piles || n_piles < 1) {
+    alert("Number of piles must be greater than 0");
+    return;
+  }
+  n_stones = document.getElementById("input-num-stones").value;
+  if (!n_stones || n_stones < 1) {
+    alert("Number of stones must be greater than 0");
+    return;
+  }
+
+  initialize(n_piles, n_stones);
+};
+
+// setup initializes window elements
+setup = () => {
+  btn_start_game = document.getElementById("btn-start-game");
+  if (!btn_start_game) {
+    console.error("btn-start-game not found");
+    return;
+  }
+  btn_start_game.addEventListener("click", onClickStartGame);
+};
+
+// main is the entry point of the script
+main = () => {
+  setup();
+};
+
+// Call main when the window is loaded
+window.onload = main;
