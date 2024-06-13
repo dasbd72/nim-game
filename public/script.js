71c8f59f54ffebdaff4466ba1792c072c3781c80
dasbd72 <twbd723@gmail.com>
2024-06-13 16:53:03 +0800

    feat: object oriented version

--- public/script.js
+++ public/script.js
@@ -1,78 +1,98 @@
 // script.js contains the logic for the Nim game
 
-// onClickStone is called when a stone is clicked
-onClickStone = (pile_id, stone_id) => {
-  console.log("Clicked stone", stone_id, "in pile", pile_id);
-
-  stone = document.getElementById(stone_id);
-  if (!stone) {
-    console.error("Stone not found");
-    return;
+// Root represents the root of web application
+class Root {
+  // constructor initializes the game
+  constructor() {
+    this.game = null;
+    this.btn_start_game = document.getElementById("btn-start-game");
+    if (!this.btn_start_game) {
+      console.error("btn-start-game not found");
+      return;
+    }
+    this.btn_start_game.addEventListener("click", () =>
+      this.onClickStartGame()
+    );
   }
-  stone.classList.toggle("stone-enabled");
-  stone.classList.toggle("stone-disabled");
-};
 
-// addStone adds a stone to the pile
-addStone = (pile, stone_id) => {
-  console.log("Adding stone");
-  stone = document.createElement("div");
-  stone.classList.add("stone");
-  stone.classList.add("stone-enabled");
-  stone.id = stone_id;
-  stone.addEventListener("click", () => onClickStone(pile.id, stone_id));
-  pile.appendChild(stone);
-};
-
-// addPile adds a pile with n_stones to the game container
-addPile = (game_container, pile_id, n_stones) => {
-  pile = document.createElement("div");
-  pile.classList.add("pile");
-  pile.id = pile_id;
-
-  for (let i = 0; i < n_stones; i++) {
-    addStone(pile, `${pile_id}-stone-${i}`);
+  // onClickStartGame is called when the start game button is clicked
+  onClickStartGame() {
+    console.log("Starting game");
+    this.game = new NimGame([3, 4, 5]);
   }
-  game_container.appendChild(pile);
-};
+}
 
-// initialize initializes the game with the number of piles and stones specified
-initialize = (n_stones_lst) => {
-  console.log("Initializing game");
-  game_container = document.getElementById("game-container");
-  if (!game_container) {
-    console.error("game-container not found");
-    return;
+// NimGame represents the Nim game
+class NimGame {
+  // constructor initializes the game with the number of piles and stones specified
+  constructor(n_stones_lst) {
+    this.n_stones_lst = n_stones_lst;
+    this.game_container = document.getElementById("game-container");
+    if (!this.game_container) {
+      console.error("game-container not found");
+      return;
+    }
+    this.game_container.innerHTML = "";
+    this.piles = [];
+    for (let i = 0; i < n_stones_lst.length; i++) {
+      let pile = new Pile(`pile-${i}`, n_stones_lst[i]);
+      this.piles.push(pile);
+      this.game_container.appendChild(pile.element);
+    }
   }
-  game_container.innerHTML = "";
+}
 
-  for (let i = 0; i < n_stones_lst.length; i++) {
-    addPile(game_container, `pile-${i}`, n_stones_lst[i]);
+// Pile represents a pile of stones
+class Pile {
+  // constructor initializes the pile with n_stones
+  constructor(id, n_stones) {
+    console.log("Creating pile", id, "with", n_stones, "stones");
+    this.element = this.#createElement(id);
+    this.stones = [];
+    for (let i = 0; i < n_stones; i++) {
+      let stone = new Stone(`${id}-stone-${i}`);
+      this.stones.push(stone);
+      this.element.appendChild(stone.element);
+    }
   }
-};
 
-// onClickStartGame is called when the start game button is clicked
-// It initializes the game with the number of piles and stones specified by the user
-onClickStartGame = () => {
-  console.log("Starting game");
-  n_stones_lst = [3, 4, 5];
-  initialize(n_stones_lst);
-};
+  // #createElement creates a pile with the specified id
+  #createElement = (id) => {
+    let element = document.createElement("div");
+    element.classList.add("pile");
+    element.id = id;
+    return element;
+  };
+}
 
-// setup initializes window elements
-setup = () => {
-  btn_start_game = document.getElementById("btn-start-game");
-  if (!btn_start_game) {
-    console.error("btn-start-game not found");
-    return;
+// Stone represents a stone
+class Stone {
+  // constructor initializes the stone
+  constructor(id) {
+    console.log("Creating stone", id);
+    this.element = this.#createElement(id);
   }
-  btn_start_game.addEventListener("click", onClickStartGame);
-};
 
-// main is the entry point of the script
-main = () => {
-  setup();
-};
+  // #createElement creates a stone with the specified id
+  #createElement = (id) => {
+    let element = document.createElement("div");
+    element = document.createElement("div");
+    element.classList.add("stone");
+    element.classList.add("stone-enabled");
+    element.id = id;
+    element.addEventListener("click", () => this.#onClick());
+    return element;
+  };
+
+  // #onClick is called when the stone is clicked
+  #onClick() {
+    console.log("Clicked stone", this.element.id);
+    this.element.classList.toggle("stone-enabled");
+    this.element.classList.toggle("stone-disabled");
+  }
+}
 
 // Call main when the window is loaded
-window.onload = main;
+window.onload = () => {
+  new Root();
+};
